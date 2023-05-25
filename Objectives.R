7e412eb8d03f2022581444aa042a8bae1fdec32d
Manuel Vargas <mvargas5897@student.palomar.edu>
2023-05-25 14:12:32 -0700

    Extracting Covid Data

---
+++ Objectives.R
@@ -0,0 +1,7 @@
+library(readr)
+library(tidyverse)
+library(cowplot)
+
+# Extract data of COVID cases
+confirmed_US <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv")
+deaths_US <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv")
